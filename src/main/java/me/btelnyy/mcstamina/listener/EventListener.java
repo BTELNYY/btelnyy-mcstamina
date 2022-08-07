7c806038a83288beaa1036c638a7721fb1f35e04
BTELNYY <74313030+BTELNYY@users.noreply.github.com>
2022-08-07 11:20:22 -0600

    Update Version, rework eventhandler

--- src/main/java/me/btelnyy/mcstamina/listener/EventListener.java
+++ src/main/java/me/btelnyy/mcstamina/listener/EventListener.java
@@ -2,35 +2,29 @@ package me.btelnyy.mcstamina.listener;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerToggleSprintEvent;
 
 import me.btelnyy.mcstamina.McStamina;
 import me.btelnyy.mcstamina.constants.ConfigData;
+/*
 import me.btelnyy.mcstamina.service.file_manager.Configuration;
 import me.btelnyy.mcstamina.service.file_manager.FileID;
+*/
+
 
 public class EventListener implements Listener {
-    private static final Configuration language = McStamina.getInstance().getFileManager().getFile(FileID.LANGUAGE).getConfiguration();
+    //private static final Configuration language = McStamina.getInstance().getFileManager().getFile(FileID.LANGUAGE).getConfiguration();
     
+    @EventHandler
     public void playerSprintToggle(PlayerToggleSprintEvent event){
         Player p = event.getPlayer();
         if(event.isSprinting()){
             DepleteHunger(p);
         }
         if(!event.isSprinting()){
-            Bukkit.getScheduler().runTaskAsynchronously(McStamina.getInstance(), new Runnable() {
-                @Override
-                public void run() 
-                {
-                    try {
-                        Thread.sleep(ConfigData.getInstance().cooldownTime);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    RegenHunger(p);
-                }
-            });
+            RegenHunger(p);
         }
     }
     public void DepleteHunger(Player target){
@@ -46,6 +40,9 @@ public class EventListener implements Listener {
                     }
                     target.setFoodLevel(target.getFoodLevel() - 1);
                 }
+                if(!target.isSprinting()){
+                    return;
+                }
             }
         });
     }
@@ -54,6 +51,11 @@ public class EventListener implements Listener {
             @Override
             public void run() 
             {
+                try {
+                    Thread.sleep(ConfigData.getInstance().cooldownTime);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 while(!target.isSprinting()){
                     try {
                         Thread.sleep(ConfigData.getInstance().depleteRate);
@@ -61,6 +63,9 @@ public class EventListener implements Listener {
                         e.printStackTrace();
                     }
                     target.setFoodLevel(target.getFoodLevel() + 1);
+                }
+                if(target.isSprinting()){
+                    return;
                 }
             }
         });
